7a7e587937b53c8652638748e0a16e73ad391b58
AlexBarba24 <alexb_sb@yahoo.com>
2025-08-06 15:11:40 -0400

    Initial Commit

---
+++ MichiganMealsAppFinal/MichiganMealsAppFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MichiganMealsAppFinalApp.swift
+//  MichiganMealsAppFinal
+//
+//  Created by Alex Barba on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct MichiganMealsAppFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MichiganMealsAppFinalTests/MichiganMealsAppFinalTests.swift
@@ -0,0 +1,17 @@
+//
+//  MichiganMealsAppFinalTests.swift
+//  MichiganMealsAppFinalTests
+//
+//  Created by Alex Barba on 8/6/25.
+//
+
+import Testing
+@testable import MichiganMealsAppFinal
+
+struct MichiganMealsAppFinalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
